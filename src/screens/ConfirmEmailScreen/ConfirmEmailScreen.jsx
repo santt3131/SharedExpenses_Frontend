6bf0369c965cde98ef46d81c3bd5e70bb4139538
santt31 <santt31@gmail.com>
2022-07-27 00:37:50 +0200

    change jsx, palette and color/ Size

---
+++ src/screens/ConfirmEmailScreen/ConfirmEmailScreen.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView, Alert } from "react-native";
+import CustomInput from "../../components/CustomInput/CustomInput";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import { useNavigation } from "@react-navigation/native";
+import { useForm } from "react-hook-form";
+
+const ConfirmEmailScreen = () => {
+	const navigation = useNavigation();
+
+	const { control, handleSubmit } = useForm();
+
+	const onConfirmPressed = () => {
+		navigation.navigate("Home");
+	};
+
+	const onResendPressed = () => {
+		Alert.alert("Info", "The code was resent. Please, check your email");
+	};
+
+	const onSignInPressed = () => {
+		navigation.navigate("SignIn");
+	};
+
+	return (
+		<ScrollView showsVerticalScrollIndicator={false}>
+			<View style={styles.root}>
+				<Text style={styles.title}>Confirm your email</Text>
+
+				<CustomInput
+					name="code"
+					placeholder="Confirmation code"
+					keyboardType="number-pad"
+					control={control}
+					rules={{
+						required: "Confirmation code is required",
+						minLength: {
+							value: 4,
+							message: "Confirmation code should be of 4 characters long",
+						},
+						maxLength: {
+							value: 4,
+							message: "Confirmation code should be of 4 characters long",
+						},
+					}}
+				/>
+
+				<CustomButton text="Confirm" onPress={handleSubmit(onConfirmPressed)} />
+
+				<CustomButton
+					text="Resend code"
+					onPress={onResendPressed}
+					type="SECONDARY"
+				/>
+
+				<CustomButton
+					text="Back to Sign In"
+					onPress={onSignInPressed}
+					type="TERTIARY"
+				/>
+			</View>
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+	root: {
+		alignItems: "center",
+		padding: 20,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		color: "#051C60",
+		margin: 10,
+	},
+	text: {
+		color: "gray",
+		marginVertical: 10,
+	},
+	link: {
+		color: "#FDB075",
+	},
+});
+
+export default ConfirmEmailScreen;
