3f25be95b1419e58a99ebfcb923c0662084b6bea
Ramiro Mejías Rodríguez <ramiro.mejias@gmail.com>
2022-09-15 18:19:14 -0700

    Friends screens almost ready

--- src/components/CustomFriendsItem/CustomFriendsItem.jsx
+++ src/components/CustomFriendsItem/CustomFriendsItem.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Platform } from "react-native";
 import { ColorPalette, Size } from "../../../appStyles";
 import CustomSpinner from "../CustomSpinner/CustomSpinner";
 import axios from "axios";
 import Global from "../../../global";
+import { useNavigation } from "@react-navigation/native";
 
 const CustomFriendsItem = ({ friends }) => {
+  const navigation = useNavigation();
+  console.log("the friend object: ", friends);
+
   if (friends === null || friends === undefined) {
     return <CustomSpinner />;
   }
@@ -54,6 +58,11 @@ const CustomFriendsItem = ({ friends }) => {
       });
   };
 
+  const onEditPress = (userName, userEmail) => {
+    console.log("recieving this name: ", userName, " and email: ", userEmail);
+    navigation.navigate("FriendsEdit", { name: userName, email: userEmail });
+  };
+
   return (
     <View style={styles.container}>
       {friends.map(({ friendName, friendEmail, invitationId }) => (
@@ -62,21 +71,47 @@ const CustomFriendsItem = ({ friends }) => {
             <Text style={styles.friendName}>{friendName}</Text>
             <Text style={styles.friendEmail}>{friendEmail}</Text>
           </View>
-          <View>
-            {invitationId !== "" && (
-              <Pressable
-                style={styles.reinviteButton}
-                onPress={() => onSendPress(friendEmail)}
-              >
-                <Text style={styles.friendButtonText}>Reinvite</Text>
-              </Pressable>
-            )}
+
+          <View
+            style={{
+              flexDirection: "row",
+              marginBottom: 5,
+              marginTop: 5,
+              justifyContent: "space-around",
+            }}
+          >
             <Pressable
               style={styles.deleteButton}
               onPress={() => onDeletePress(friendEmail)}
             >
               <Text style={styles.friendButtonText}>Delete</Text>
             </Pressable>
+            <Pressable
+              style={styles.reinviteButton}
+              onPress={() => onEditPress(friendName, friendEmail)}
+            >
+              <Text style={styles.friendButtonText}>Edit</Text>
+            </Pressable>
+            {invitationId === "" ? (
+              <Pressable
+                style={[
+                  styles.reinviteButton,
+                  { backgroundColor: ColorPalette.background },
+                ]}
+              >
+                <Text style={styles.friendButtonText}>Reinvite</Text>
+              </Pressable>
+            ) : (
+              <Pressable
+                style={[
+                  styles.reinviteButton,
+                  { backgroundColor: ColorPalette.primaryBlue },
+                ]}
+                onPress={() => onSendPress(friendEmail)}
+              >
+                <Text style={styles.friendButtonText}>Reinvite</Text>
+              </Pressable>
+            )}
           </View>
         </View>
       ))}
@@ -99,37 +134,62 @@ const styles = StyleSheet.create({
     paddingRight: 10,
   },
   friendContainer: {
-    display: "flex",
-    flexDirection: "row",
-    marginBottom: 10,
-  },
-  friendData: {
     flex: 1,
     flexDirection: "column",
     paddingLeft: 10,
     paddingTop: 3,
     paddingBottom: 3,
     borderLeftColor: ColorPalette.primaryBlue,
-    borderLeftWidth: 6,
-    backgroundColor: ColorPalette.veryLightGrey,
+    borderLeftWidth: 3,
+
+    borderTopEndRadius: 5,
+    borderBottomEndRadius: 5,
+    backgroundColor: ColorPalette.background,
+    marginBottom: 10,
+  },
+  friendData: {
+    flex: 5,
+    flexDirection: "column",
+    paddingLeft: 10,
+    paddingTop: 3,
+    paddingBottom: 3,
+
     borderTopEndRadius: 4,
     borderBottomEndRadius: 4,
-    marginRight: 2,
+    marginRight: 0,
   },
   friendName: {
     color: ColorPalette.primaryBlack,
     fontSize: Size.xm,
     fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 5,
   },
   friendEmail: {
     color: ColorPalette.primaryGray,
     fontSize: Size.ls,
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flex: 2,
   },
   reinviteButton: {
+    flex: 1 / 3,
+    height: 40,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: ColorPalette.primaryBlue,
-    borderRadius: 4,
+    backgroundColor: ColorPalette.primaryGreen,
+    borderRadius: 5,
+    marginHorizontal: 5,
+    marginBottom: 2,
+  },
+  deleteButton: {
+    flex: 1 / 3,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: ColorPalette.primaryRouge,
+    borderRadius: 5,
+    marginHorizontal: 3,
     marginBottom: 2,
   },
   friendButtonText: {
@@ -137,14 +197,6 @@ const styles = StyleSheet.create({
     padding: 5,
     textTransform: "uppercase",
     fontWeight: "bold",
-  },
-  deleteButton: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: ColorPalette.primaryRed,
-    borderRadius: 4,
-    width: 75,
   },
 });
 
