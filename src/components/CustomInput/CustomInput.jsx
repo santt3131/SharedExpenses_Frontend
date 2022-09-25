0ab77d2a6a1ece1ed1e8ed10ea06a97f204f4212
Ramiro Mejías Rodríguez <ramiro.mejias@gmail.com>
2022-09-25 09:40:57 -0700

    editable prop added to the component

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -4,79 +4,81 @@ import { Controller } from "react-hook-form";
 import { ColorPalette, Size } from "../../../appStyles";
 
 const CustomInput = ({
-	control,
-	name,
-	rules = {},
-	placeholder,
-	secureTextEntry,
-	keyboardType,
-	autoCapitalize,
-	defaultValue
+  control,
+  name,
+  rules = {},
+  placeholder,
+  secureTextEntry,
+  keyboardType,
+  autoCapitalize,
+  defaultValue,
+  editable,
 }) => {
-	return (
-		<Controller
-			control={control}
-			name={name}
-			rules={rules}
-			keyboardType={keyboardType}
-			autoCapitalize={autoCapitalize}
-			render={({
-				field: { value, onChange, onBlur },
-				fieldState: { error },
-			}) => (
-				<>
-					<View
-						style={[
-							styles.container,
-							{
-								borderColor: error
-									? ColorPalette.primaryRed
-									: ColorPalette.veryLightGrey,
-							},
-						]}
-					>
-						<TextInput
-							value={defaultValue??value}
-							onChangeText={onChange}
-							onBlur={onBlur}
-							autoCapitalize={autoCapitalize ? autoCapitalize : "none"}
-							placeholder={placeholder}
-							placeholderTextColor={ColorPalette.primaryGray}
-							style={styles.input}
-							secureTextEntry={secureTextEntry}
-							keyboardType={keyboardType}
-						/>
-					</View>
-					{error && (
-						<Text
-							style={{ color: ColorPalette.primaryRed, alignSelf: "stretch" }}
-						>
-							{error.message || "Error"}
-						</Text>
-					)}
-				</>
-			)}
-		/>
-	);
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      keyboardType={keyboardType}
+      autoCapitalize={autoCapitalize}
+      render={({
+        field: { value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <>
+          <View
+            style={[
+              styles.container,
+              {
+                borderColor: error
+                  ? ColorPalette.primaryRed
+                  : ColorPalette.veryLightGrey,
+              },
+            ]}
+          >
+            <TextInput
+              value={defaultValue ?? value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              autoCapitalize={autoCapitalize ? autoCapitalize : "none"}
+              placeholder={placeholder}
+              placeholderTextColor={ColorPalette.primaryGray}
+              style={styles.input}
+              secureTextEntry={secureTextEntry}
+              keyboardType={keyboardType}
+              editable={editable}
+            />
+          </View>
+          {error && (
+            <Text
+              style={{ color: ColorPalette.primaryRed, alignSelf: "stretch" }}
+            >
+              {error.message || "Error"}
+            </Text>
+          )}
+        </>
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: ColorPalette.primaryWhite,
-		width: "100%",
+  container: {
+    backgroundColor: ColorPalette.primaryWhite,
+    width: "100%",
 
-		borderColor: ColorPalette.veryLightGrey,
-		borderWidth: 1,
-		borderRadius: 5,
+    borderColor: ColorPalette.veryLightGrey,
+    borderWidth: 1,
+    borderRadius: 5,
 
-		paddingHorizontal: 10,
-		marginVertical: 5,
-	},
-	input: {
-		height: 48,
-		color: ColorPalette.primaryBlack,
-		fontSize: Size.xm,
-	},
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  input: {
+    height: 48,
+    color: ColorPalette.primaryBlack,
+    fontSize: Size.xm,
+  },
 });
 
 export default CustomInput;
